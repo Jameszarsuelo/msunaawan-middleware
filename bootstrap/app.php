8519b79dbda909a325cff18b50bf13532be53fb7
cyrusjameszarsuelo <cyrusjames.cjz@gmail.com>
2025-03-04 22:28:43 +0800

    cors issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    ->withMiddleware([
+        CorsMiddleware::class,  // ✅ Directly add middleware here
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
